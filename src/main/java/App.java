b1eab47d114069148bdde4e95c50f618da13c8f4
Andruschenko Igor <andruschenko-ip@gamma-center.ru>
2016-12-16 22:58:13 +0300

    14-12-2016 - Start Lesson 7 - Enum

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,14 +3,18 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.EventListener;
+import java.util.Map;
 
-/**
+/*
  * user
  * 13.12.2016
  */
 public class App {
     Client client;
-    ConsoleEventLogger eventLogger;
+    //ConsoleEventLogger eventLogger;
+    EventLogger logger;
+    EventLogger defaultLogger;
+    Map<EventType, EventLogger> loggers;
 
     public static void main(String[] args) {
         ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
@@ -22,16 +26,34 @@ public class App {
         //app.logEvent("Come event for user 1");
         //app.logEvent("Come event for user 2");
 
-        Event testEvent1 = (Event) ctx.getBean("event");
-        Event testEvent2 = (Event) ctx.getBean("event");
-        testEvent1.setMsg("Come event for user 1");
-        testEvent2.setMsg("Come event for user 2");
+        //Event testEvent1 = (Event) ctx.getBean("event");
+        //Event testEvent2 = (Event) ctx.getBean("event");
+        //testEvent1.setMsg("Come event for user 1");
+        //testEvent2.setMsg("Come event for user 2");
 
-        app.logEvent(testEvent1);
-        app.logEvent(testEvent1);
+        app.logEvent(EventType.ERROR,"Come event for user 1\"");
+        app.logEvent(EventType.INFO,"Come event for user 2");
+        //app.logEvent(null,testEvent2);
 
         ctx.close();
     }
+
+    void logEvent(EventType type, String msg) {
+        EventLogger logger = loggers.get(type);
+        if (logger == null) {
+            logger = defaultLogger;
+        }
+        logger.logEvent(event);
+
+
+    }
+
+    public App(Client client, EventLogger eventLogger, Map<EventType, EventLogger> loggers) {
+        this.client = client;
+        this.loggers = loggers;
+
+    }
+
 
     void logEvent(Event event) {
         //String message = msg.replaceAll(client.getId(), client.getFullName());
@@ -39,8 +61,11 @@ public class App {
 
     }
 
-    public App(Client client, ConsoleEventLogger eventLogger) {
+ /*
+    public App(Client client, ConsoleEventLogger eventLogger, EventType eventType) {
         this.client = client;
         this.eventLogger = eventLogger;
-    }
+
+    }*/
+
 }
